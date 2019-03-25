b63adff6391faf6c4d7743dd1284fe61556545b5
Tobias Schulz-Hess <tobias.schulz-hess@xing.com>
2019-03-25 13:58:15 +0100

    Fixed scroll to top behaviour and replaced js animations with css animations

--- js/main.js
+++ js/main.js
@@ -38,39 +38,20 @@
    	if (nav.hasClass('mobile')) nav.fadeOut('fast');      
   	});	
 
-  
-  	/* Smooth Scrolling
-  	------------------------------------------------------ */
-  	$('.smoothscroll').on('click', function (e) {
-	 	
-	 	e.preventDefault();
 
-   	var target = this.hash,
-    	$target = $(target);
-
-    	$('html, body').stop().animate({
-       	'scrollTop': $target.offset().top
-      }, 800, 'swing', function () {
-      	window.location.hash = target;
-      });
-
-  	}); 
 
   	
   	/*	Back To Top Button
 	------------------------------------------------------- */
 	var pxShow = 300; //height on which the button will show
-	var fadeInTime = 400; //how slow/fast you want the button to show
-	var fadeOutTime = 400; //how slow/fast you want the button to hide
-	var scrollSpeed = 300; //how slow/fast you want the button to scroll to top. can be a value, 'slow', 'normal' or 'fast'
 
    // Show or hide the sticky footer button
 	$(window).scroll(function() {
 
 		if ($(window).scrollTop() >= pxShow) {
-			$("#go-top").show(fadeInTime);
+			$("#go-top").css("opacity", "1");
 		} else {
-			$("#go-top").hide(fadeOutTime);
+			$("#go-top").css("opacity", "0");
 		}
 
 	});  
