a2113ea88b7cb889f30bd9a38cf3d395dfa18d74
Tobias Schulz-Hess <tobias.schulz-hess@xing.com>
2019-03-26 21:28:00 +0100

    Updated font awesome to 5.8.1 and added images to 1st grade subtractions, code cleanup

--- js/main.js
+++ js/main.js
@@ -66,3 +66,82 @@ function getParameterByName(name, url) {
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
+
+function add(max, section) {
+	var already = new Array();
+	var howmuch = Math.min(max, 100);
+	var iFormat = new Intl.NumberFormat('de-DE');
+
+	for (var i = 0; i < howmuch; i++) {
+		do {
+			var a = Math.floor(Math.random() * max) + 1;
+			var b = Math.floor(Math.random() * (max - a)) + 1;
+			var key = a + "-" + b;
+		} while (already.indexOf(key) !== -1);
+
+		$(section).append("<div class=\"t\">" + iFormat.format(a) + " + " + iFormat.format(b) + " = _____ </div>");
+		already.push(key);
+	}
+}
+
+function sub(max, section) {
+	var already = new Array();
+	var howmuch = Math.min(max, 100);
+	var iFormat = new Intl.NumberFormat('de-DE');
+
+	for (var i = 0; i < howmuch; i++) {
+		do {
+			var a = Math.floor(Math.random() * max * 0.9) + (max * 0.1);
+			var b = Math.floor(Math.random() * (a - (max * 0.01))) + 1;
+			var key = a + "-" + b;
+			if (a === b) {
+				already.push(key);
+			}
+		} while (already.indexOf(key) !== -1);
+
+		$(section).append("<div class=\"t\">" + iFormat.format(a) + " - " + iFormat.format(b) + " = _____ </div>");
+		already.push(key);
+	}
+}
+
+var icons = [["crow", "dove"],
+	["fish", "fish fa-flip-horizontal"],
+	["frog", "frog fa-flip-horizontal"],
+	["horse", "horse fa-flip-horizontal"],
+	["hippo", "hippo fa-flip-horizontal"],
+	["motorcycle", "motorcycle fa-flip-horizontal"],
+	["plane", "plane fa-flip-horizontal"],
+	["running", "running fa-flip-horizontal"],
+	["shuttle-van", "shuttle-van fa-flip-horizontal"],
+	["skating", "skating fa-flip-horizontal"],
+	["swimmer", "swimmer fa-flip-horizontal"],
+	["tractor", "tractor fa-flip-horizontal"],
+	["truck", "truck fa-flip-horizontal"],
+	["walking", "walking fa-flip-horizontal"]];
+
+function subWithIcons(max, section, count) {
+	for (var i = 0; i < count; i++) {
+		var iconIndex = Math.floor(Math.random() * icons.length);
+		var a = Math.floor(Math.random() * max * 0.9) + (max * 0.1);
+		do {
+			b = Math.floor(Math.random() * (a - (max * 0.01))) + 1;
+		} while (b < 1 || b >= a);
+
+		var classesA = "";
+		for (var j = 0; j < a; j++) {
+			classesA += "<i class=\"fas fa-" + icons[iconIndex][0] + "\"></i>"
+		}
+		var classesB = "";
+		for (var j = 0; j < b; j++) {
+			classesB += "<i class=\"fas fa-" + icons[iconIndex][1] + "\"></i>"
+		}
+		classesB += "<i class=\"fas fa-align-left fa-separator\"></i>";
+		for (var j = b; j < a; j++) {
+			classesB += "<i class=\"fas fa-" + icons[iconIndex][0] + "\"></i>"
+		}
+
+		$(section).append("<div class=\"tFull group\"><div class=\"tImages tImages1\">" + classesA + "</div>"
+			+ "<div class=\"tImages tImages2\">" + classesB + "</div></div>"
+			+ "<div class=\"tFull group\"><p>______ - _____ = _____</p></div>");
+	}
+}
